2f2584eead637f4f5f1bd0b2c900b229a05f66a5
OraniumTech <oraniumtech@gmail.com>
2023-05-14 22:44:58 +0530

    New commit

---
+++ src/test/java/pack1/Demo2Test.java
@@ -0,0 +1,15 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class Demo2Test {
+
+	
+
+	@Test
+	public void tc1()
+	{
+		System.out.println("Test case method 1 ");
+	}
+
+}
---
+++ src/test/java/pack1/Sample.java
@@ -0,0 +1,14 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+	
+	@Test
+	public void tc1()
+	{
+		System.out.println("Test case method 1 ");
+	}
+
+}
